6fc298126e766a859c5700cbb49ffc5b9184266d
Manav sagar <manavsagar46@gmail.com>
2025-08-15 21:07:03 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement("h1", {id:"heading", abc:"xyz"}, "Hello from react");
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 Tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 Tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);
+// console.log(typeof heading)
+// console.log(root)
+
+root.render(parent);
